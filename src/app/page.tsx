d0120b739d2b39b1b453cb799e304bf9612f773a
dilmithp <dilmithp@outlook.com>
2025-07-24 08:39:31 +0530

    better-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,38 @@
 import {Button} from "@/components/ui/button";
 import {useState} from "react";
 import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import { authClient } from "@/lib/auth-client";
+
 
 export default function Home() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
 
+    const onSubmit = async () => {
+        authClient.signUp.email({
+            name,
+            email,
+            password
+        },
+            {
+                onError: () => {
+                    window.alert("Error occurred");
+                },
+                onSuccess: () => {
+                    window.alert("User created successfully");
+                }
+            })
+    }
+
   return (
-      <div>
+      <div className={'p-4 flex flex-col gap-4 max-w-md mx-auto'}>
+            <Label className="text-2xl">Create User</Label>
           <Input placeholder={name} value={name} onChange={(e) => setName(e.target.value)} />
           <Input placeholder={email} value={email} onChange={(e) => setEmail(e.target.value)} />
           <Input placeholder={password} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <Button>Create User</Button>
+          <Button onClick={onSubmit}>Create User</Button>
       </div>
   )
 }
