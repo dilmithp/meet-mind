2c4b9d6e145af172f8942b9e74197b5bb913b91e
dilmithp <dilmithp@outlook.com>
2025-08-19 13:51:44 +0530

    19 meetings filters

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -6,6 +6,7 @@ import {NewAgentDialog} from "@/modules/agents/ui/components/new-agent-dialog";
 import {UseAgentsFilter} from "@/modules/agents/hooks/use-agents-filter";
 import {AgentsSearchFilter} from "@/modules/agents/ui/components/agents-search-filter";
 import {DEFAULT_PAGE} from "@/constants";
+import {ScrollArea, ScrollBar} from "@/components/ui/scroll-area";
 
 
 export const AgentsListHeader = () => {
@@ -31,15 +32,18 @@ export const AgentsListHeader = () => {
                     New Agent
                 </Button>
             </div>
-            <div className={'flex items-center gap-x-2 p-1'}>
-                <AgentsSearchFilter/>
-                {isAnyFilterModified && (
-                    <Button variant={'outline'} size={'sm'} onClick={onClearFilters}>
-                        <XCircleIcon />
-                        Clear
-                    </Button>
-                )}
-            </div>
+            <ScrollArea>
+                <div className={'flex items-center gap-x-2 p-1'}>
+                    <AgentsSearchFilter/>
+                    {isAnyFilterModified && (
+                        <Button variant={'outline'} size={'sm'} onClick={onClearFilters}>
+                            <XCircleIcon />
+                            Clear
+                        </Button>
+                    )}
+                </div>
+                <ScrollBar orientation={'horizontal'}/>
+            </ScrollArea>
         </div>
         </>
     )
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -7,13 +7,25 @@ import {ErrorState} from "@/components/error-state";
 import {DataTable} from "@/components/data-table";
 import {columns} from "@/modules/meetings/ui/components/columns";
 import {EmptyState} from "@/components/empty-state";
+import {useRouter} from "next/navigation";
+import {UseMeetingsFilter} from "@/modules/meetings/hooks/use-meetings-filter";
+import {DataPagination} from "@/components/data-pagination";
 
 export const MeetingsView = () => {
     const trpc = useTRPC();
-    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+    const router = useRouter();
+    const [filters, setFilters] = UseMeetingsFilter();
+    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({
+        ...filters,
+    }));
     return (
         <div className={'flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4'}>
-            <DataTable data={data.items} columns={columns}/>
+            <DataTable data={data.items} columns={columns}
+                       onRowClick={(row) => router.push(`/meetings/${row.id}`)}/>
+            <DataPagination
+                page={filters.page}
+                totalPages={data.totalPages}
+                onPageChange={(page) => setFilters({page})}/>
             {data.items.length === 0 && (
                 <EmptyState title={"Create Your first meeting"} description={"Create a meeting to connect with your AI Support Agent"}/>
             )}
