439e15c07c603671aee2d881434938e386d39c19
dilmithp <dilmithp@outlook.com>
2025-08-21 06:48:28 +0530

    22 video call

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -7,8 +7,30 @@ import {DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE} from "@/c
 import {TRPCError} from "@trpc/server";
 import {meetingsInsertSchema, meetingsUpdateSchema} from "@/modules/meetings/schemas";
 import {MeetingStatus} from "@/modules/meetings/types";
+import {streamVideo} from "@/lib/stream-video";
+import {generateAvatarUri} from "@/lib/avatar";
 
 export const meetingsRouter = createTRPCRouter({
+    generateToken: protectedProcedure.mutation(async ({ctx}) => {
+        await streamVideo.upsertUsers([
+            {
+                id: ctx.auth.user.id,
+                name: ctx.auth.user.name,
+                image: ctx.auth.user.image ?? generateAvatarUri({seed: ctx.auth.user.name, variant: "initials"}),
+                role: 'admin',
+            },
+        ]);
+
+        const expirationTime = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
+        const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+        const token = streamVideo.generateUserToken({
+            user_id: ctx.auth.user.id,
+            exp: expirationTime,
+            validity_in_seconds: issuedAt,
+        });
+        return token;
+    }),
     remove: protectedProcedure
         .input(z.object({id: z.string()}))
         .mutation(async ({input, ctx}) => {
@@ -21,7 +43,7 @@ export const meetingsRouter = createTRPCRouter({
                     ),
                 )
                 .returning();
-            if(!removedMeeting) {
+            if (!removedMeeting) {
                 throw new TRPCError({code: 'NOT_FOUND', message: 'Meeting not found'});
             }
             return removedMeeting;
@@ -39,7 +61,7 @@ export const meetingsRouter = createTRPCRouter({
                     ),
                 )
                 .returning();
-            if(!updatedMeeting) {
+            if (!updatedMeeting) {
                 throw new TRPCError({code: 'NOT_FOUND', message: 'Meeting not found'});
             }
             return updatedMeeting;
@@ -55,6 +77,42 @@ export const meetingsRouter = createTRPCRouter({
                 } as InferInsertModel<typeof meetings>)
                 .returning();
 
+            const call = streamVideo.video.call("default", createdMeeting.id);
+            await call.create({
+                data: {
+                    created_by_id: ctx.auth.user.id,
+                    custom: {
+                        meetingId: createdMeeting.id,
+                        meetingName: createdMeeting.name,
+                    },
+                    settings_override: {
+                        transcription: {
+                            language: "en",
+                            mode: "auto-on",
+                            closed_caption_mode: "auto-on",
+                        },
+                        recording: {
+                            mode: "auto-on",
+                            quality: "1080p",
+                        },
+                    },
+                },
+            });
+            const [existingAgent] = await db
+                .select()
+                .from(agent)
+                .where(eq(agent.id, createdMeeting.agentId));
+            if (!existingAgent) {
+                throw new TRPCError({code: 'NOT_FOUND', message: 'Agent not found'});
+            }
+            await streamVideo.upsertUsers([
+                {
+                    id: existingAgent.id,
+                    name: existingAgent.name,
+                    image: generateAvatarUri({seed: existingAgent.name, variant: "botttsNeutral"}),
+                    role: 'user',
+                },
+            ])
             return createdMeeting;
         }),
     getOne: protectedProcedure.input(z.object({id:z.string()})).query(async ({input, ctx}) => {
