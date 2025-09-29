6aaf3b1d4e7f4c8b9cb454b277237eceb4fff365
Vishwa Wijekoon <vishwamadusanka1022@gmail.com>
2025-09-29 11:07:04 +0530

    30-transcript-note

--- src/modules/meetings/schemas.ts
+++ src/modules/meetings/schemas.ts
@@ -1,4 +1,5 @@
-import {z} from "zod";
+import { z } from "zod";
+
 
 export const meetingsInsertSchema = z.object({
     name: z.string().min(1, "Name is required"),
@@ -6,5 +7,21 @@ export const meetingsInsertSchema = z.object({
 });
 
 export const meetingsUpdateSchema = meetingsInsertSchema.extend({
-    id: z.string().min(1, {message: "ID is required"}),
-})
+    id: z.string().min(1, { message: "ID is required" }),
+});
+
+
+export const noteInsertSchema = z.object({
+    meetingId: z.string(),
+    note: z.string().min(3, { message: "Note must be at least 3 characters long." }),
+    email: z.string().email({ message: "Please enter a valid email address." }),
+});
+
+export const noteUpdateSchema = z.object({
+    id: z.string(),
+    note: z.string().min(3, { message: "Note must be at least 3 characters long." }),
+});
+
+export const noteDeleteSchema = z.object({
+    id: z.string(),
+});
