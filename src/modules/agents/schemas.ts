c5b0bd2435960572c3232628cf2098902a73142e
Sachintha Praneeth <it23595262@my.sliit.lk>
2025-09-30 18:50:39 +0530

    31 agent form validation

--- src/modules/agents/schemas.ts
+++ src/modules/agents/schemas.ts
@@ -1,10 +1,15 @@
 import {z} from "zod";
 
 export const agentsInsertSchema = z.object({
-    name: z.string().min(1, "Name is required"),
+    name: z.string()
+        .min(1, "Name is required")
+        .regex(
+            /^[a-zA-Z0-9 ]+$/,
+            "Name can only contain letters, numbers, and spaces."
+        ),
     instructions: z.string().min(1, "Instructions are required"),
 });
 
 export const agentsUpdateSchema = agentsInsertSchema.extend({
     id: z.string().min(1, {message: "ID is required"}),
-})
+});
